25e67cb393ff88ed9aac5966051b81df7d78fd11
Moni Garr <Moni Garr>
2017-03-12 22:17:17 -0400

    completed, tested & verified working as expected: navigation, play button, go into room, look around, fires happening, look & listen to levitating orbs that play simon says with player, player clicks orbs like simon says game and floats forward to option for game restart or look around at the night sky or look behind to see the fire blazing inside the room with mysterious levitating orbs.

--- PuzzlerStarter/Assets/Dynamic Flickering Light/FlickeringLight.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:126916bc95c5bce0f52facbda5e48945cbc671a1a074f0969ef580dd5f513520
-size 3713
---
+++ PuzzlerStarter/Assets/PyroParticles/Prefab/Materials/AdditiveTintable.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:315468d5462bc0ec6f080205c171b9a8a2e84bde08b1b62d52c8c67a2cd1a093
+size 2417
